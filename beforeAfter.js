15697330f0379f2501f003ea142b82643ed3905a
Jason <jsato03@gmail.com>
2018-04-18 00:23:05 -0700

    minor refactoring, rename splitPhrase variable

--- beforeAfter.js
+++ beforeAfter.js
@@ -3,7 +3,7 @@ const HashTable = require('./hashTable.js');
 let input = [
   // Basic examples
   'clutter will help you clean',
-  'clean your room!',
+  'clean out your house!',
   'hole in the wall',
   'wall street journal',
 
@@ -24,23 +24,22 @@ const beforeAfter = phrases => {
 
   // For each phrase, populate hash table with keys (first word) and values (remainder of phrase)
   for (let phrase of phrases) {
-    let splitPhrase = phrase.split(' ');
-    storage.insert(splitPhrase[0], splitPhrase.slice(1).join(' '));
+    let phraseArr = phrase.split(' ');
+    storage.insert(phraseArr[0], phraseArr.slice(1).join(' '));
   }
 
   // Check for valid before/after phrase pairs and add to joinedPhrases array
   for (let phrase of phrases) {
     let splitPhrase = phrase.split(' ');
     let lastWord = splitPhrase[splitPhrase.length - 1];
-    let values = storage.retrieve(lastWord);
-    if (values.length) {
-      for (let value of values) {
+    let matchingValues = storage.retrieve(lastWord);
+    if (matchingValues.length) {
+      for (let value of matchingValues) {
         let newPhrase = phrase + ' ' + value;
         joinedPhrases = [...joinedPhrases, newPhrase];
       }
     }
   }
-
   return joinedPhrases;
 };
 
